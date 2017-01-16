f8e78fd27566f8a14f0226050fb9b69e7c9b00cc
Orla Walsh <orlawalsh@outlook.com>
2017-01-16 19:25:08 +0000

    Add gems & Foodlog form

--- app/controllers/foodlogs_controller.rb
+++ app/controllers/foodlogs_controller.rb
@@ -1,2 +1,43 @@
 class FoodlogsController < ApplicationController
+	before_action :find_foodlog, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def show
+
+	end
+
+	def new
+		@foodlog = Foodlog.new
+	end
+
+	def create
+		@foodlog = Foodlog.new(foodlog_params)
+		if @foodlog.save
+			redirect_to @foodlog
+		else
+			render'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private 
+
+	def foodlog_params
+		params.require(:foodlog).permit(:date, :mealtype, :meal, :mood, :level)
+	end
+
+	def find_foodlog
+		@foodlog = Foodlog.find(params[:id])
+	end
+
 end
